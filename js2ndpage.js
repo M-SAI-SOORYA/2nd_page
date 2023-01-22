26407c82fa902c8e3f82fb89c5e858254e6f9706
Marri Sai Soorya <96968484+M-SAI-SOORYA@users.noreply.github.com>
2023-01-22 22:00:31 +0530

    Add files via upload

---
+++ js2ndpage.js
@@ -0,0 +1,66 @@
+
+
+function func() {
+  var target = document.getElementById("filesss");
+  var check=document.getElementById("flexCheckDefault1");
+  if (check.checked===true) {
+    target.style.display = "block";
+  }
+  else {
+    target.style.display = "none";
+
+  }
+}
+
+
+
+function da(){
+  var element = document.body;
+   element.classList.toggle("darkmode");
+}
+function funcalert(){
+  setTimeout(alert("Your Grievance has been recored!"),11000);
+}
+//
+// function func_all(){
+//   let targecy=document.getElementById("Privacy");
+//   let targems=document.getElementById("termsc");
+//
+//   if(targecy.clicked == true){
+//     targecy.style.className("mystyle").display="block";
+//
+//
+//   }
+//   else if(targems.clicked == true){
+//     targems.style.className("mystyle").display="block";
+//
+//
+//
+//   }
+//   else{
+//     alert("U didnt do any thing");
+//
+//   }
+
+
+// function funcpr(){
+//
+//
+//   let element=document.getElementById("Privacy");
+//   element.classList.toggle("mystyle");
+//
+// }
+// function functer(){
+//   let element=document.getElementById("termsc");
+//   element.classList.toggle("mystyle");
+//
+// }
+// function funchelp(){
+//   let element=document.getElementById("helpp");
+//   element.classList.toggle("mystyle");
+//
+// }
+// function funconta(){
+//   document.getElementById("contacte").style.borderColor = "red";
+//
+// }
